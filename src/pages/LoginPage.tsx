ecce998dc7189e7bd8dff2c81e2ce4a003cc643c
Peter Pardo <peterpardo123@gmail.com>
2025-09-18 23:18:53 +0800

    Initial setup of login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,10 +1,22 @@
-import { Button } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 
 function LoginPage() {
   return (
     <div>
       <h1>Login Page</h1>
-      <Button variant="contained">Click me!</Button>
+      <TextField
+        id="outlined-basic"
+        label="email"
+        variant="outlined"
+        type="email"
+      />
+      <TextField
+        id="outlined-basic"
+        label="password"
+        variant="outlined"
+        type="password"
+      />
+      <Button variant="contained">Login</Button>
     </div>
   );
 }
